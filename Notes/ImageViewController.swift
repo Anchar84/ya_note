b9c4c29af7ec38b2f75a2a2eee8f4c000a62bec7
Georgy Bastrakov <eg@dartit.ru>
2019-08-01 20:32:03 +0500

    image pick

--- Notes/ImageViewController.swift
+++ Notes/ImageViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ImageViewController: UIViewController {
 
     var imgViews = [UIImageView]()
+    let imagePicker = UIImagePickerController()
+    var imgCounter = 0
     
     @IBOutlet weak var scroll: UIScrollView!
     override func viewDidLoad() {
@@ -22,7 +24,9 @@ class ImageViewController: UIViewController {
             imgViews.append(imgView)
             scroll.addSubview(imgView)
         }
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Галерея"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addClicked(_:)))
     }
     
     override func viewDidLayoutSubviews() {
@@ -31,9 +35,80 @@ class ImageViewController: UIViewController {
             iView.frame.size = scroll.frame.size
             iView.frame.origin.x = scroll.frame.width * CGFloat(index)
             iView.frame.origin.y = 0
+            imgCounter += 1
         }
         let w = scroll.frame.width * CGFloat(imgViews.count)
         scroll.contentSize = CGSize(width: w, height: scroll.frame.height)
     }
+    
+    @objc func addClicked (_ sender: Any) {
+        selectImage()
+    }
 
 }
+
+extension ImageViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func selectImage(){
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func openCamera()
+    {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        {
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallery()
+    {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            let imgView = UIImageView(image: pickedImage)
+            imgView.contentMode = .scaleToFill
+            imgViews.append(imgView)
+            scroll.addSubview(imgView)
+            
+            imgView.frame.size = scroll.frame.size
+            imgView.frame.origin.x = scroll.frame.width * CGFloat(imgCounter)
+            imgView.frame.origin.y = 0
+            
+            imgCounter += 1
+            let w = scroll.frame.width * CGFloat(imgCounter)
+            scroll.contentSize = CGSize(width: w, height: scroll.frame.height)
+
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
